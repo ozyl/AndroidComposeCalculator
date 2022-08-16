ccf4b08f9e67116d944a28887654d7b1a8797201
Yongle <zylvip@163.com>
2022-08-17 01:34:07 +0800

    点击效果完善

--- app/src/main/java/cn/okzyl/studyjamscompose/MainActivity.kt
+++ app/src/main/java/cn/okzyl/studyjamscompose/MainActivity.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
-
 package cn.okzyl.studyjamscompose
 
 import android.os.Bundle
@@ -8,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
@@ -23,6 +23,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerInputScope
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -35,6 +38,9 @@ import cn.okzyl.studyjamscompose.ui.theme.Background
 import cn.okzyl.studyjamscompose.ui.theme.Orange
 import cn.okzyl.studyjamscompose.ui.theme.StudyJamsComposeTheme
 import cn.okzyl.studyjamscompose.ui.theme.selectColor
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,8 +48,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             StudyJamsComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Calculator()
                 }
             }
@@ -56,8 +64,10 @@ fun Calculator() {
     var calculateState by remember {
         mutableStateOf(CalculateState())
     }
-    Column(Modifier
-        .background(Background), verticalArrangement = Arrangement.Bottom) {
+    Column(
+        Modifier
+            .background(Background), verticalArrangement = Arrangement.Bottom
+    ) {
         Box(
             Modifier
                 .fillMaxWidth()
@@ -67,9 +77,11 @@ fun Calculator() {
         ) {
             InputShow(calculateState)
         }
-        Column(Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.5f)) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+        ) {
             buttons.forEachIndexed { oneIndex, it ->
                 Row(Modifier.weight(1f)) {
                     it.forEachIndexed { twoIndex, it ->
@@ -77,7 +89,8 @@ fun Calculator() {
                             modifier = Modifier
                                 .weight(1f)
                                 .fillMaxHeight(),
-                            it) {
+                            it
+                        ) {
                             calculateState.onInput(it)?.run {
                                 calculateState = this
                             }
@@ -91,16 +104,22 @@ fun Calculator() {
 
 @Composable
 private fun InputShow(calculateState: CalculateState) {
-    Column(modifier = Modifier
-        .verticalScroll(rememberScrollState(), reverseScrolling = true),
-        horizontalAlignment = Alignment.End) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState(), reverseScrolling = true),
+        horizontalAlignment = Alignment.End
+    ) {
         val annotationText = buildAnnotatedString {
             calculateState.list.forEachIndexed { index, it ->
                 pushStringAnnotation(tag = index.toString(), annotation = it.text)
-                withStyle(style = SpanStyle(color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp,
-                    background = if (it.editing) selectColor else Color.Unspecified)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        background = if (it.editing) selectColor else Color.Unspecified
+                    )
+                ) {
                     append(it.text)
                 }
                 pop()
@@ -119,9 +138,11 @@ private fun InputShow(calculateState: CalculateState) {
                 }
             }
         }
-        Text(text = calculateState.result.toString(),
+        Text(
+            text = calculateState.result.toString(),
             color = Color.White,
-            fontSize = 100.sp)
+            fontSize = 100.sp
+        )
     }
 }
 
@@ -131,25 +152,69 @@ fun CalculatorButton(modifier: Modifier, buttonModel: ButtonModel, onClick: () -
     var down by remember {
         mutableStateOf(false)
     }
+    val interactionSource = remember { MutableInteractionSource() }
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    suspend fun PointerInputScope.pointerInput()  {
+        detectTapGestures(onPress = {
+            down = true
+            context.vibrator(0L to 10L, amplitude = 50)
+
+
+            val notShowInteraction = buttonModel.type == ButtonType.CALCULATE
+            var press :PressInteraction.Press?=null
+            if (!notShowInteraction){
+                press = PressInteraction.Press(it)
+                interactionSource.emit(press)
+            }
+            var job:Job?=null
+            //Continuous delete
+            if (buttonModel.type == ButtonType.DELETE){
+                var first = true
+                job = scope.launch {
+                    while (true){
+                        delay(if (first) 500 else 70)
+                        first = false
+                        context.vibrator(0L to 10L, amplitude = 50)
+                        onClick.invoke()
+                    }
+                }
+            }
+            val result = tryAwaitRelease()
+            job?.cancel()
+
+            press?.run {
+                interactionSource.emit(PressInteraction.Release(this))
+            }
+
+            if (result){
+                onClick.invoke()
+            }
+            down = false
+        })
+    }
     Box(
         modifier
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(radius = 30.dp)
-            ) { onClick.invoke() },
+            .indication(interactionSource,rememberRipple(radius = 30.dp))
+            .pointerInput(Unit,PointerInputScope::pointerInput),
         contentAlignment = Alignment.Center,
     ) {
-        val size =
-            animateFloatAsState(targetValue = if (down) 0.85f else 1f,
-                animationSpec = tween(durationMillis = 150))
-        Box(modifier = Modifier
-            .graphicsLayer(scaleX = size.value, scaleY = size.value)
-            .size(60.dp)
-            .clip(CircleShape)
-            .background(if (buttonModel.type == ButtonType.CALCULATE) Orange else Color.Unspecified),
-            contentAlignment = Alignment.Center) {
+        val sizeAnimate =
+            animateFloatAsState(
+                targetValue = if (down) 0.85f else 1f,
+                animationSpec = tween(durationMillis = 50)
+            )
+        Box(
+            modifier = Modifier
+                .graphicsLayer(scaleX = sizeAnimate.value, scaleY = sizeAnimate.value)
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(if (buttonModel.type == ButtonType.CALCULATE) Orange else Color.Unspecified),
+            contentAlignment = Alignment.Center
+        ) {
             if (buttonModel.res != null) {
-                Image(painter = painterResource(id = buttonModel.res),
+                Image(
+                    painter = painterResource(id = buttonModel.res),
                     contentDescription = null,
                     Modifier
                         .size(35.dp)
